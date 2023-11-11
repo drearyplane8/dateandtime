6527f9b8d4e141f28df65942020c9df8568358b4
lottie <cjcmcghee1@sheffield.ac.uk>
2023-11-11 14:59:12 +0000

    efhf

--- python-folders/app.py
+++ python-folders/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import *
+from flask_cors import CORS
 import datetime
-import os
+
 
 app = Flask(__name__)
+CORS(app)
 @app.route("/")
 def get_current_month():
     month = datetime.datetime.now().month
--- webpage/index.js
+++ webpage/index.js
@@ -1,10 +1,24 @@
 function getDateTime() {
     let date = new Date();
     document.getElementById("day").textContent = date.getDay().toLocaleString(undefined, {minimumIntegerDigits: 2});
-    document.getElementById("month").textContent = date.getMonth().toLocaleString(undefined, {minimumIntegerDigits: 2});
+    
     document.getElementById("year").textContent = date.getFullYear();
 
     document.getElementById("hour").textContent = date.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2});
     document.getElementById("minute").textContent = date.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2});
     document.getElementById("second").textContent = date.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2});
+
+    makeMonthRequest();
+}
+
+function makeMonthRequest() {
+    let request = new XMLHttpRequest();
+    request.addEventListener("load", handleMonthRequest);
+    request.open("GET", "http://127.0.0.1:5000");
+    request.send();
+}
+
+function handleMonthRequest() {
+    console.log(this.responseText);
+    document.getElementById("month").textContent = this.responseText.toLocaleString(undefined, {minimumIntegerDigits: 2});
 }
