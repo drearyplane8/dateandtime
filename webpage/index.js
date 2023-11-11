b6d5e0040832b60de7a47950e8dbca125726168c
lottie <cjcmcghee1@sheffield.ac.uk>
2023-11-11 16:29:15 +0000

    oogh

--- webpage/index.js
+++ webpage/index.js
@@ -4,21 +4,34 @@ function getDateTime() {
     
     document.getElementById("year").textContent = date.getFullYear();
 
-    document.getElementById("hour").textContent = date.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2});
     document.getElementById("minute").textContent = date.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2});
     document.getElementById("second").textContent = date.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2});
 
     makeMonthRequest();
+    makeHourRequest();
 }
 
 function makeMonthRequest() {
     let request = new XMLHttpRequest();
     request.addEventListener("load", handleMonthRequest);
-    request.open("GET", "http://127.0.0.1:5000");
+    request.open("GET", "http://127.0.0.1:5000/month");
     request.send();
 }
 
 function handleMonthRequest() {
     console.log(this.responseText);
     document.getElementById("month").textContent = this.responseText.toLocaleString(undefined, {minimumIntegerDigits: 2});
+}
+
+function makeHourRequest() {
+    let request = new XMLHttpRequest();
+    request.addEventListener("load", handleHourRequest);
+    request.open("GET", "http://127.0.0.1:5000/hour");
+    request.send();
+    console.log("made hour request")
+}
+
+function handleHourRequest() {
+    console.log(this.responseText)
+    document.getElementById("hour").textContent = this.responseText.toLocaleString(undefined, {minimumIntegerDigits: 2});
 }
