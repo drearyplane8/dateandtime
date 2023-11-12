effda4e59a03b6605b3222b64f2a86d1a20fbd35
Kile <69253692+Kile@users.noreply.github.com>
2023-11-12 12:27:00 +0000

    a

--- python-folders/app.py
+++ python-folders/app.py
@@ -72,31 +72,31 @@ def hours():
     seconds = until.seconds % 60
     return "There are {} days, {} hours, {} minutes, and {} seconds until the next Taylor Swift concert.".format(until.days, hours, minutes, seconds)
 
-# @app.route("/m")
-# def get_current_month():
-#     val = os.popen("date /t").read() 
-#     return str(val)[3:5]
+@app.route("/m")
+def get_current_month():
+    val = os.popen("date /t").read() 
+    return str(val)[3:5]
 
-# @app.route("/get/current/hour")
-# def get_current_hour():
-#     hour = subprocess.run("../win32/a.exe").returncode
-#     print(hour)
-#     return str(hour)
+@app.route("/get/current/hour")
+def get_current_hour():
+    hour = subprocess.run("../win32/a.exe").returncode
+    print(hour)
+    return str(hour)
 
-# @app.route("/day")
-# def get_current_day():
+@app.route("/day")
+def get_current_day():
 
-#     executor = ThreadPoolExecutor(max_workers = 2)
-#     val = executor.submit(start_client)
-#     executor.submit(start_server)
-#     print(val.result)
-#     return str(val.result())
+    executor = ThreadPoolExecutor(max_workers = 2)
+    val = executor.submit(start_client)
+    executor.submit(start_server)
+    print(val.result)
+    return str(val.result())
 
-# def start_client():
-#     return subprocess.run("java Client localhost 54321", encoding="utf-8", cwd="../sockets", capture_output=True).stdout
+def start_client():
+    return subprocess.run("java Client localhost 54321", encoding="utf-8", cwd="../sockets", capture_output=True).stdout
 
-# def start_server():
-#     subprocess.run("java Server 54321", cwd="../sockets")  
+def start_server():
+    subprocess.run("java Server 54321", cwd="../sockets")  
 
 if __name__ == "__main__":
     app.run(debug=True)
